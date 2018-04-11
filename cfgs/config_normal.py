3f158993c63919052694e552f3a4ca98473b5ce6
JesseYang <jesse.yang1985@gmail.com>
2018-04-11 22:26:54 +0800

    visualize part

--- cfgs/config_normal.py
+++ cfgs/config_normal.py
@@ -60,7 +60,7 @@ _.sigma = 7
 _.grid_y = int(_.img_y / _.stride)
 _.grid_x = int(_.img_x / _.stride)
 
-_.thre = 1
+_.thre1 = 0.1
 
 _.ch_heats = 18 + 1 # 18个parts 第19个(遍历不会执行到)表示是非人体区域
 _.ch_vectors = 38
--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 import functools
+import matplotlib
+from scipy.ndimage.filters import gaussian_filter
 import os
 from pathlib import Path
 
@@ -126,6 +128,47 @@ def predict(args):
 
     cv2.imwrite('heatmap_shown.jpg', img_with_heatmap)
 
+    all_peaks = []
+    peak_counter = 0
+
+    for part in range(cfg.ch_heats - 1):
+        map_ori = heatmap_avg[:, :, part]
+        map_flt = gaussian_filter(map_ori, sigma=3)
+
+        map_left = np.zeros(map_flt.shape)
+        map_left[1:,:] = map_flt[:-1,:]
+        map_right = np.zeros(map_flt.shape)
+        map_right[:-1,:] = map_flt[1:,:]
+        map_up = np.zeros(map_flt.shape)
+        map_up[:,1:] = map_flt[:,:-1]
+        map_down = np.zeros(map_flt.shape)
+        map_down[:,:-1] = map_flt[:,1:]
+
+        peaks_binary = np.logical_and.reduce((map_flt>=map_left, map_flt>=map_right, map_flt>=map_up, map_flt>=map_down, map_flt>cfg.thre1))
+        peaks = zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]) # note reverse
+        peaks_with_score = [x + (map_ori[x[1],x[0]],) for x in peaks]
+        id = range(peak_counter, peak_counter + len(peaks_with_score))
+        peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
+
+        all_peaks.append(peaks_with_score_and_id)
+        peak_counter += len(peaks_with_score)
+
+    # visualize
+    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
+              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
+              [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+    cmap = matplotlib.cm.get_cmap('hsv')
+    
+    canvas = np.copy(img) # B,G,R order
+    
+    for i in range(18):
+        rgba = np.array(cmap(1 - i/18. - 1./36))
+        rgba[0:3] *= 255
+        for j in range(len(all_peaks[i])):
+            cv2.circle(canvas, all_peaks[i][j][0:2], 4, colors[i], thickness=-1)
+    
+    to_plot = cv2.addWeighted(img, 0.3, canvas, 0.7, 0)
+    cv2.imwrite('part.jpg', to_plot)
 
 if __name__ == '__main__':
 
